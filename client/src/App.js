4d2192cb588a3cfaa5fa5c730256685fa945064e
aveesp <aveesp@gmail.com>
2019-12-20 16:43:19 +0530

    updated Hashrouter for build.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Details from "./components/Details";
 import "./App.css";
 
